4c32bc4af5f9360e9ee503ba32e7e65116d77784
therikisingh <rikisingh302@gmail.com>
2026-09-23 13:42:32 -0700

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kripa.app',
+  appName: 'Kripa',
+  webDir: 'www'
+};
+
+export default config;
